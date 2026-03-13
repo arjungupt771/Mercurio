a0f311d7198621af5069671130151e87b7393ec1
Arjun Gupta <118122439+arjungupt771@users.noreply.github.com>
2026-03-13 19:31:11 +0530

    Remove Firebase config from firebase.js
    Removed sensitive Firebase configuration details.

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpc2NSIw3BzucyaJPboQUJz9WBiVPOHcw",
-  authDomain: "mercurio-finance-tracker.firebaseapp.com",
-  projectId: "mercurio-finance-tracker",
-  storageBucket: "mercurio-finance-tracker.firebasestorage.app",
-  messagingSenderId: "991280424226",
-  appId: "1:991280424226:web:a671f9b5b20580a49ab17b",
-  measurementId: "G-TM52TH9MG3"
+
 };
 
 const app = initializeApp(firebaseConfig);
